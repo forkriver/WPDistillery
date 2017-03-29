aa4971174b11da1f0c1c7867e29cfc0b62b87798
Patrick Johanneson <johanneson@brandonu.ca>
2017-03-29 15:37:36 -0500

    Added Multisite options (and added in supporting content, eg, .htaccess files) (see #1)

--- wpdistillery/wpdistillery.sh
+++ wpdistillery/wpdistillery.sh
@@ -71,6 +71,7 @@ fi
 
 cd $CONF_wpfolder
 
+ 
 # INSTALL WORDPRESS
 if $CONF_setup_wp ; then
   printf "${BRN}[=== INSTALL WORDPRESS ===]${NC}\n"
@@ -78,8 +79,22 @@ if $CONF_setup_wp ; then
   wp core download --locale=$CONF_wplocale --version=$CONF_wpversion
   printf "${BLU}»»» creating wp-config...${NC}\n"
   wp core config --dbname=$CONF_db_name --dbuser=$CONF_db_user --dbpass=$CONF_db_pass --dbprefix=$CONF_db_prefix --locale=$CONF_wplocale
-  printf "${BLU}»»» installing wordpress...${NC}\n"
-  wp core install --url=$CONF_wpsettings_url --title="$CONF_wpsettings_title" --admin_user=$CONF_admin_user --admin_password=$CONF_admin_password --admin_email=$CONF_admin_email --skip-email
+  if $CONF_setup_wp_multisite ; then
+    printf "${BLU}»»» installing WordPress Multisite...${NC}\n"
+    if $CONF_setup_wp_multisite_subdomain ; then
+      wp core multisite-install --url=$CONF_wpsettings_url --subdomains --title="$CONF_wpsettings_title" --admin_user=$CONF_admin_user --admin_password=$CONF_admin_password --admin_email=$CONF_admin_email --skip-email
+      cp $CONF_wp_multisite_htaccess/multisite-subdomain.htaccess ./.htaccess
+    else
+      wp core multisite-install --url=$CONF_wpsettings_url --title="$CONF_wpsettings_title" --admin_user=$CONF_admin_user --admin_password=$CONF_admin_password --admin_email=$CONF_admin_email --skip-email
+      cp $CONF_wp_multisite_htaccess/multisite-subfolder.htaccess ./.htaccess
+
+    fi
+    # wp core multisite-install etc
+    # also add in the .htaccess file somehow
+  else
+    printf "${BLU}»»» installing wordpress...${NC}\n"
+    wp core install --url=$CONF_wpsettings_url --title="$CONF_wpsettings_title" --admin_user=$CONF_admin_user --admin_password=$CONF_admin_password --admin_email=$CONF_admin_email --skip-email
+  fi
   wp user update 1 --first_name=$CONF_admin_first_name --last_name=$CONF_admin_last_name
 else
   printf "${BLU}>>> skipping WordPress installation...${NC}\n"
